aee22ce1c1293790365fc550a835d9eeaeef5ada
Liam <coolchild1230@gmail.com>
2026-04-23 17:11:14 +1000

    modular

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import pygame, sys
 
 pygame.init()
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1024, 768
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-titleFont = pygame.font.Font("smash_hit/Smash Hit.ttf", size=67)
-mainFont = pygame.font.Font("smash_hit/Smash Hit.ttf", size=20)
-finish = False
+titleFont = pygame.font.Font("smash_hit/Smash Hit light.ttf", size=67)
+mainFont = pygame.font.Font("smash_hit/Smash Hit.ttf", size=40)
+
 WHITE = (255, 255, 255)
 SELECTED = (255, 255, 0)
 selected = 0
-while finish != True:
 
-    for event in pygame.event.get():
+def main_menu(events) -> str:
+    global selected
+
+    for event in events:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 if selected > 0:
@@ -20,11 +22,11 @@ while finish != True:
             elif event.key == pygame.K_DOWN:
                 if selected < 4:
                     selected += 1
-            elif event.key == pygame.K_ESCAPE:
-                finish = True
-
-        if event.type == pygame.QUIT:
-            finish = True
+            elif event.key == pygame.K_RETURN:
+                if selected == 0:
+                    return "game"
+                elif selected == 4:
+                    return "quit"
 
     title = titleFont.render("liams game", True, WHITE)
     screen.blit(title, (100, 100))
@@ -33,18 +35,36 @@ while finish != True:
     screen.blit(start, (100, 300))
 
     endless = mainFont.render("start endless", True, SELECTED if selected == 1 else WHITE)
-    screen.blit(endless, (100, 320))
+    screen.blit(endless, (100, 330))
     
     highscore = mainFont.render("high scores", True, SELECTED if selected == 2 else WHITE)
-    screen.blit(highscore, (100, 340))
+    screen.blit(highscore, (100, 360))
 
     options = mainFont.render("options", True, SELECTED if selected == 3 else WHITE)
-    screen.blit(options, (100, 360))
+    screen.blit(options, (100, 390))
 
     quitpls = mainFont.render("quit", True, SELECTED if selected == 4 else WHITE)
-    screen.blit(quitpls, (100, 380))
+    screen.blit(quitpls, (100, 420))
+
+    return "main_menu"
+
+
+state = "main_menu"
+
+while True:
+    events = pygame.event.get()
+    for i in events:
+        if i.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    if state == "main_menu":
+        state = main_menu(events)
+    elif state == "quit":
+        pygame.quit()
+        sys.exit()
+    else:
+        print("implementing later")
+        state = "quit"
 
     pygame.display.flip()
-
-pygame.quit()
-sys.exit()
