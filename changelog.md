0575cea16d5b2fee7967760cdd884ebadde1e84c
Liam <coolchild1230@gmail.com>
2026-04-24 22:22:40 +1000

    added bg and made it #retro

--- changelog.md
+++ changelog.md
@@ -1,3 +1,4 @@
 23/04 - made initial menu gui
 24/04 - removed type annotations (ill put it back later but i cant be bothered now), added animations to the menu, changing project to delta time to avoid speed mistakes
 24/04 - added character select and difficulty select
+24/04 - added discord rpc and made the ui look better
--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-import pygame, sys, json
+import pygame, sys, json, discordrpc
+
+try:
+    rpc = discordrpc.RPC(app_id=1497206637994315827)
+    rpc.set_activity( state="testing my game", details="ezez")
+except:
+    pass
+    
+def load_file(f):
+    with open(f, "r") as f:
+        return json.load(f)
+
+def save_file(f, data):
+    with open(f, "w") as f:
+        json.dump(data, f)
+
+config = load_file("config.json")
 
 pygame.init()
 pygame.mixer.init()
 
-WIDTH, HEIGHT = 1024, 768
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+WIDTH, HEIGHT = 1000, 720
+WINDOW_W = 1280
+screen_offset = (WINDOW_W - WIDTH) // 2
+
+if config["fullscreen"]:
+   window = pygame.display.set_mode((WINDOW_W, HEIGHT), pygame.FULLSCREEN) 
+else:
+    window = pygame.display.set_mode((WINDOW_W, HEIGHT))
+
+def are_we_fs():
+    global window, screen
+
+    if config["fullscreen"]:
+        window = pygame.display.set_mode((WINDOW_W, HEIGHT), pygame.FULLSCREEN)
+    else:
+        window = pygame.display.set_mode((WINDOW_W, HEIGHT))
+
+    screen_offset = (WINDOW_W - WIDTH) // 2
+    screen = window.subsurface((screen_offset, 0, WIDTH, HEIGHT))
+
+screen = window.subsurface((screen_offset, 0, WIDTH, HEIGHT))
 
 titleFont = pygame.font.Font("smash_hit/Smash Hit light.ttf", 67)
 mainFont = pygame.font.Font("smash_hit/Smash Hit.ttf", 40)
@@ -20,7 +55,7 @@ state = "main_menu"
 music_playing = False
 
 menu_offsets = [0, 0, 0, 0, 0]
-options_menu_offsets = [0, 0, 0]
+options_menu_offsets = [0, 0, 0, 0, 0]
 char_select_offsets = [0, 0, 0]
 char_select_entered = False
 char_scroll = 0
@@ -28,15 +63,6 @@ diff_entered = False
 diff_selected = 0
 diff_scroll = 0
 
-def load_file(f):
-    with open(f, "r") as f:
-        return json.load(f)
-
-def save_file(f, data):
-    with open(f, "w") as f:
-        json.dump(data, f)
-
-config = load_file("config.json")
 
 def should_music_be_on():
     global music_playing
@@ -87,7 +113,12 @@ def main_menu(events, dt):
                     return "options"
                 elif selected == 4:
                     return "quit"
-
+            elif event.key == pygame.K_ESCAPE:
+                if selected != 4:
+                    selected = 4
+                    menu_offsets[4] = -80
+                else:
+                    return "quit"
     for i in range(5):
         menu_offsets[i] *= (0.0001 ** dt)
 
@@ -104,10 +135,10 @@ def difficulty_select(events, dt):
     global diff_selected, diff_scroll
 
     DIFFICULTY = [
-        {"name": "easy", "color": (0, 255, 0), "desc": "for casual play"},
-        {"name": "normal", "color": (255, 255, 255), "desc": "balanced"},
-        {"name": "hard", "color": (255, 140, 0), "desc": "challenging"},
-        {"name": "extreme", "color": (255, 0, 0), "desc": "not fair"},
+        {"name": "easy", "color": (0, 255, 0), "desc": "never played a stg"},
+        {"name": "normal", "color": (255, 255, 0), "desc": "played a stg before!"},
+        {"name": "hard", "color": (255, 140, 0), "desc": "you play stgs often?"},
+        {"name": "extreme", "color": (255, 0, 0), "desc": "not fair..."},
     ]
 
     for event in events:
@@ -129,9 +160,9 @@ def difficulty_select(events, dt):
     spacing = 350
     target = diff_selected * spacing
 
-    diff_scroll += (target - diff_scroll) * (1 - pow(0.001, dt * 8))
+    diff_scroll += (target - diff_scroll) * (1 - pow(0.001, dt * 2))
 
-    screen.blit(titleFont.render("select difficulty", True, WHITE), (WIDTH // 3, 120))
+    screen.blit(titleFont.render("select difficulty", True, WHITE), (WIDTH // 4, 120))
 
     center_x = WIDTH // 2
     y = HEIGHT // 2
@@ -194,10 +225,10 @@ def char_select(events, dt):
         char_select_offsets[i] *= pow(0.001, dt * 6)
         if abs(char_select_offsets[i]) < 0.5:
             char_select_offsets[i] = 0
-    screen.blit(titleFont.render("select agent", True, WHITE), (WIDTH / 3, 100))
-    box_y = (768 - box_h) // 2
+    screen.blit(titleFont.render("select agent", True, WHITE), (WIDTH // 4, 100))
+    box_y = (HEIGHT - box_h) // 2
     total_w = len(CHARS) * box_w + (len(CHARS) - 1) * gap
-    start_x = (1024 - total_w) // 2
+    start_x = (WIDTH - total_w) // 2
 
     k = 0
     while k < len(CHARS):
@@ -234,7 +265,7 @@ def options(events, dt):
             if event.key == pygame.K_UP and options_selected > 0:
                 options_selected -= 1
                 options_menu_offsets[options_selected] = -80
-            elif event.key == pygame.K_DOWN and options_selected < 2:
+            elif event.key == pygame.K_DOWN and options_selected < 3:
                 options_selected += 1
                 options_menu_offsets[options_selected] = -80
             elif event.key == pygame.K_LEFT:
@@ -242,30 +273,45 @@ def options(events, dt):
                     config["lives"] -= 1
                 if options_selected == 1:
                     config["music"] = "off"
+                if options_selected == 2:
+                    config["fullscreen"] = False
             elif event.key == pygame.K_RIGHT:
                 if options_selected == 0 and config["lives"] < 5:
                     config["lives"] += 1
                 if options_selected == 1:
                     config["music"] = "on"
-            elif event.key == pygame.K_RETURN:
                 if options_selected == 2:
+                    config["fullscreen"] = True
+            elif event.key == pygame.K_RETURN:
+                if options_selected == 3:
                     save_file("config.json", config)
                     should_music_be_on()
+                    are_we_fs()
                     kick_main()
                     return "main_menu"
             elif event.key == pygame.K_ESCAPE:
                 kick_main()
                 return "main_menu"
-    for i in range(3):
+    for i in range(4):
         options_menu_offsets[i] *= (0.0001 ** dt)
-
+    screen.blit(titleFont.render("options", True, WHITE), (WIDTH // 4, 120))
     screen.blit(mainFont.render(f"lives: {config['lives']}", True, SELECTED if options_selected == 0 else WHITE), (200 + options_menu_offsets[0], 300))
     screen.blit(mainFont.render(f"music: {config['music']}", True, SELECTED if options_selected == 1 else WHITE), (200 + options_menu_offsets[1], 340))
-    screen.blit(mainFont.render("back", True, SELECTED if options_selected == 2 else WHITE), (200 + options_menu_offsets[2], 380))
+    screen.blit(mainFont.render(f"fullscreen: {config['fullscreen']}", True, SELECTED if options_selected == 2 else WHITE), (200 + options_menu_offsets[2], 380))
+    screen.blit(mainFont.render("back", True, SELECTED if options_selected == 3 else WHITE), (200 + options_menu_offsets[3], 420))
 
     return "options"
 
+def game(events, dt):
+    global config, diff_selected, char_selected
+    print( config, diff_selected, char_selected)
+    return "quit"
+
 clock = pygame.time.Clock()
+window_bg = pygame.image.load("bg.jpg").convert()
+screen_bg = pygame.image.load("mainbg.jpg").convert()
+window_bg = pygame.transform.scale(window_bg, (WINDOW_W, HEIGHT))
+screen_bg = pygame.transform.scale(screen_bg, (WIDTH, HEIGHT))
 
 while True:
     dt = clock.tick() / 1000
@@ -277,7 +323,8 @@ while True:
             pygame.quit()
             sys.exit()
 
-    screen.fill((0, 0, 0))
+    window.blit(window_bg, (0, 0))
+    screen.blit(screen_bg, (0, 0))
 
     if state == "main_menu":
         state = main_menu(events, dt)
@@ -287,6 +334,8 @@ while True:
         state = difficulty_select(events, dt)
     elif state == "char_select":
         state = char_select(events, dt)
+    elif state == "game":
+        state = game(events, dt)
     elif state == "quit":
         pygame.quit()
         sys.exit()
